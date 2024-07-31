16c1f6e8cb81b600834991f4562b8c368a971ef8
oh-ok-i-get-it <hpeery17@gmail.com>
2024-07-31 01:58:00 -0500

    more notes for myself

--- pyLzr.py
+++ pyLzr.py
@@ -6,6 +6,13 @@ to read microphone input, then send MIDI signal to virtual MIDI device
 which will be connected to SoundSwitch until I can build or find a 
 good DMX protocol API
 
+AudioVisualizer -> virtual MIDI device/keyboard -> SoundSwitch -> DMX adapter -> laser
+
+
+Note to self: decide to either bring over old PyLZR code from PyCharm or 
+start from scratch on GUI (possibly better to restart because might not 
+use OpenDMX or OLA - but maybe try OLA with ArtNet adapter?)
+
 """
 
 
