6dc175fa76d8581b2950e087eaaeb1adfd9c752f
oh-ok-i-get-it <hpeery17@gmail.com>
2024-07-31 01:50:21 -0500

    commented basic progress idea

---
+++ pyLzr.py
@@ -0,0 +1,13 @@
+"""
+Main file for PyLZR-
+
+probably first going to start with building a audio spectrum analyzer 
+to read microphone input, then send MIDI signal to virtual MIDI device
+which will be connected to SoundSwitch until I can build or find a 
+good DMX protocol API
+
+"""
+
+
+
+
