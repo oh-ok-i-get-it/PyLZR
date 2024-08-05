dc32a1df113c9465fa417df9abfba9814deba2d5
oh-ok-i-get-it <hpeery17@gmail.com>
2024-08-05 18:01:32 -0500

    both midi and visualizer implemented to same file program

--- pyLzr.py
+++ pyLzr.py
@@ -21,6 +21,9 @@ screen_width = 500
 screen_height = 500
 caption = "PyLZR"
 tick_rate = 60
+BLACK = [0, 0, 0]
+WHITE = [255, 255, 255]
+running = True
 
 # PYAUDIO
 CHUNK = 1024
@@ -106,22 +109,150 @@ def draw_sine_wave(amplitude):
     pygame.display.flip()
 
 
+
 ### RUNNING
 with midiout:
     #variables 
-    running = True
-
-    #running
-    while running == True:
+    while running:
         screen.fill(BLACK)
 
         for event in pygame.event.get():
             
             #CHECK CLOSE
             if event.type == pygame.QUIT:
-                done = True
+                running = False
 
+            ### KEYBOARD CONTROL
+            if event.type == pygame.KEYDOWN:
+                #SPACE BAR
+                if event.key == pygame.K_SPACE:
+                    print("SPACE")
+                    press_MIDI_note(60)
 
+                #KEYBOARD
+                if event.key == pygame.K_1:
+                    print("1")
+                    press_MIDI_note(61)
+                if event.key == pygame.K_2:
+                    print("2")
+                    press_MIDI_note(62)
+                if event.key == pygame.K_3:
+                    print("3")
+                    press_MIDI_note(63)
+                if event.key == pygame.K_4:
+                    print("4")
+                    press_MIDI_note(64)
+                if event.key == pygame.K_5:
+                    print("5")
+                    press_MIDI_note(65)
+                if event.key == pygame.K_6:
+                    print("6")
+                    press_MIDI_note(66)
+                if event.key == pygame.K_7:
+                    print("7")
+                    press_MIDI_note(67)
+                if event.key == pygame.K_8:
+                    print("8")
+                    press_MIDI_note(68)
+                if event.key == pygame.K_9:
+                    print("9")
+                    press_MIDI_note(69)
+                if event.key == pygame.K_0:
+                    print("0")
+                    press_MIDI_note(70)
+                
+                if event.key == pygame.K_q:
+                    print("q")
+                    press_MIDI_note(71)
+                if event.key == pygame.K_w:
+                    print("w")
+                    press_MIDI_note(72)
+                if event.key == pygame.K_e:
+                    print("e")
+                    press_MIDI_note(73)
+                if event.key == pygame.K_r:
+                    print("r")
+                    press_MIDI_note(74)
+                if event.key == pygame.K_t:
+                    print("t")
+                    press_MIDI_note(75)
+                if event.key == pygame.K_y:
+                    print("y")
+                    press_MIDI_note(76)
+                if event.key == pygame.K_u:
+                    print("u")
+                    press_MIDI_note(77)
+                if event.key == pygame.K_i:
+                    print("i")
+                    press_MIDI_note(78)
+                if event.key == pygame.K_o:
+                    print("o")
+                    press_MIDI_note(79)
+                if event.key == pygame.K_p:
+                    print("p")
+                    press_MIDI_note(80)
+                
+                if event.key == pygame.K_a:
+                    print("a")
+                    press_MIDI_note(81)
+                if event.key == pygame.K_s:
+                    print("s")
+                    press_MIDI_note(82)
+                if event.key == pygame.K_d:
+                    print("d")
+                    press_MIDI_note(83)
+                if event.key == pygame.K_f:
+                    print("f")
+                    press_MIDI_note(84)
+                if event.key == pygame.K_g:
+                    print("g")
+                    press_MIDI_note(85)
+                if event.key == pygame.K_h:
+                    print("h")
+                    press_MIDI_note(86)
+                if event.key == pygame.K_j:
+                    print("j")
+                    press_MIDI_note(87)
+                if event.key == pygame.K_k:
+                    print("k")
+                    press_MIDI_note(88)
+                if event.key == pygame.K_l:
+                    print("l")
+                    press_MIDI_note(89)
+                if event.key == pygame.K_SEMICOLON:
+                    print(";")
+                    press_MIDI_note(90)
+                
+                if event.key == pygame.K_z:
+                    print("z")
+                    press_MIDI_note(91)
+                if event.key == pygame.K_x:
+                    print("x")
+                    press_MIDI_note(92)
+                if event.key == pygame.K_c:
+                    print("c")
+                    press_MIDI_note(93)
+                if event.key == pygame.K_v:
+                    print("v")
+                    press_MIDI_note(94)
+                if event.key == pygame.K_b:
+                    print("b")
+                    press_MIDI_note(95)
+                if event.key == pygame.K_n:
+                    print("n")
+                    press_MIDI_note(96)
+                if event.key == pygame.K_m:
+                    print("m")
+                    press_MIDI_note(97)
+                if event.key == pygame.K_COMMA:
+                    print(",")
+                    press_MIDI_note(98)
+                if event.key == pygame.K_PERIOD:
+                    print(".")
+                    press_MIDI_note(99)
+                if event.key == pygame.K_SLASH:
+                    print("/")
+                    press_MIDI_note(100)
 
         #adjust and dampen amplitude height
         amplitude_adjustment = get_mic_input_level() / DAMPEN_AMP
@@ -135,5 +266,4 @@ with midiout:
         #limit runs per second to 60
         clock.tick(tick_rate)
 
-
-
+del midiout
